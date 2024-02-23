c3d28cf9063246ce0048b7220a62a835ca1d8e2c
ldg <tmg123456789@hotmail.com>
2024-02-23 16:44:09 +0800

    add some comments to queue test

--- 数据结构/【郝斌】-数据结构入门/code/queue.c
+++ 数据结构/【郝斌】-数据结构入门/code/queue.c
@@ -28,90 +28,104 @@ bool out_queue(QUEUE *, int *pVal); // 出队
 int main(void)
 {
     QUEUE Q;
+    int val = 0;
 
     init(&Q);
-	en_queue(&Q, 1);
-	en_queue(&Q, 2);
-	en_queue(&Q, 3);
-	en_queue(&Q, 4);
-	en_queue(&Q, 5);
-	en_queue(&Q, 6);
-	en_queue(&Q, 7);
-	en_queue(&Q, 8);
-	traverse_queue(&Q);
+    en_queue(&Q, 1);
+    en_queue(&Q, 2);
+    en_queue(&Q, 3);
+    en_queue(&Q, 4);
+    en_queue(&Q, 5);
+    en_queue(&Q, 6);
+    en_queue(&Q, 7); // 队列已满，不再入队
+    en_queue(&Q, 8); // 队列已满，不再入队
+    out_queue(&Q, &val);
+    out_queue(&Q, &val);
+    out_queue(&Q, &val);
+    out_queue(&Q, &val);
+    out_queue(&Q, &val);
+    out_queue(&Q, &val);
+    out_queue(&Q, &val); // 队列已空，不再出队
+    en_queue(&Q, 6); // rear变为0，重新入队
+    en_queue(&Q, 7);
+    en_queue(&Q, 8);
+    out_queue(&Q, &val); // front变为0，重新出队
+    out_queue(&Q, &val);
+
+    traverse_queue(&Q);
 
     return 0;
 }
 
 void init(QUEUE *pQ)
 {
-	pQ->pBase = (int *)malloc(sizeof(int) * 6); // 创建一块有6个int大小的内存空间
-	pQ->front = 0;
-	pQ->rear = 0;
+    pQ->pBase = (int *)malloc(sizeof(int) * 6); // 创建一块有6个int大小的内存空间
+    pQ->front = 0;
+    pQ->rear = 0;
 }
 
 bool full_queue(QUEUE *pQ)
 {
-	if(((pQ->rear + 1) % 6) == (pQ->front))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if (((pQ->rear + 1) % 6) == (pQ->front)) // 通过存放n-1个队列成员的方式来区分满队列和空队列的区别
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool en_queue(QUEUE *pQ, int val)
 {
-	if(full_queue(pQ))
-	{
-		return false;
-	}
-	else
-	{
-		pQ->pBase[pQ->rear] = val;
-		pQ->rear = (pQ->rear + 1) % 6; // rear + 1取余6，使队尾的索引取值范围保持在0~5，从而实现循环队列
-		
-		return true;
-	}	
+    if (full_queue(pQ))
+    {
+        return false;
+    }
+    else
+    {
+        pQ->pBase[pQ->rear] = val;
+        pQ->rear = (pQ->rear + 1) % 6; // rear + 1取余6，使队尾的索引取值范围保持在0~5，从而实现循环队列
+
+        return true;
+    }
 }
 
 void traverse_queue(QUEUE *pQ)
 {
-	int i = pQ->front;
-	
-	while(i != pQ->rear)
-	{
-		printf("%d ", pQ->pBase[i]);
-		i = (i + 1) % 6;
-	}
-	printf("\n");
+    int i = pQ->front;
+
+    while (i != pQ->rear)
+    {
+        printf("%d ", pQ->pBase[i]);
+        i = (i + 1) % 6;
+    }
+    printf("\n");
 }
 
 bool empty_queue(QUEUE *pQ)
 {
-	if((pQ->front) == (pQ->rear))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if ((pQ->front) == (pQ->rear))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool out_queue(QUEUE *pQ, int *pVal)
 {
-	if(empty_queue(pQ))
-	{
-		return false;
-	}
-	else
-	{
-		*pVal = pQ->pBase[front]; 
-		pQ->front = (pQ->front + 1) % 6;// front + 1取余6，使队首的索引取值范围保持在0~5，从而实现循环队列
-		
-		return true;
-	}
+    if (empty_queue(pQ))
+    {
+        return false;
+    }
+    else
+    {
+        *pVal = pQ->pBase[pQ->front];
+        pQ->front = (pQ->front + 1) % 6; // front + 1取余6，使队首的索引取值范围保持在0~5，从而实现循环队列
+
+        return true;
+    }
 }
